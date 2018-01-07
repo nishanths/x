e130b50bfe0f500718d9855c483bc8b359b9ad64
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2018-01-06 20:12:35 -0500

    tweak revproxy api

--- revproxy/revproxy.go
+++ revproxy/revproxy.go
@@ -2,46 +2,44 @@
 package revproxy
 
 import (
-	"errors"
 	"net/http"
 	"net/url"
 )
 
 // TargetFunc returns the target URL to use for the incoming request.
-type TargetFunc func(r *http.Request) (*url.URL, error)
+type TargetFunc func(r *http.Request) *url.URL
 
 // ByHost determines the target URL's host based on the incoming request's
 // host. m maps the host of an incoming request to its target host.
 // The scheme for the target URL is always "http".
+//
+// If the incoming request's host isn't in the supplied map,
+// the returned TargetFunc will panic.
 func ByHost(m map[string]string) TargetFunc {
-	return func(r *http.Request) (*url.URL, error) {
+	return func(r *http.Request) *url.URL {
 		v, ok := m[r.Host]
 		if !ok {
 			// Unknown host. Either the map should be fixed
 			// to include an entry for the host, or DNS settings
 			// for the domain should be updated to not point
 			// to the IP.
-			return nil, errors.New("unknown host: " + r.Host)
+			panic("unknown host: " + r.Host)
 		}
 		return &url.URL{
 			Scheme:   "http",
 			Host:     v,
 			Path:     r.URL.Path,
 			RawQuery: r.URL.RawQuery,
-		}, nil
+		}
 	}
 }
 
 // MultiHostDirector returns a function that can be used as the Director
 // in httputil.ReverseProxy. The supplied TargetFunc determines which target
-// URL the incoming request should directed to. If TargetFunc returns an
-// error, the director panics with that error.
+// URL the incoming request should directed to.
 func MultiHostDirector(t TargetFunc) func(r *http.Request) {
 	return func(r *http.Request) {
-		target, err := t(r)
-		if err != nil {
-			panic(err)
-		}
+		target := t(r)
 		r.URL.Scheme = target.Scheme
 		r.URL.Host = target.Host
 		r.URL.Path = target.Path
